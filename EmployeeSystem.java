4e72e6c2fe71d921ed45416d71c5eb0db212275f
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-25 14:44:10 +0530

    Making List of Object from File

--- EmployeeSystem.java
+++ EmployeeSystem.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeSystem {
@@ -13,18 +14,18 @@ public class EmployeeSystem {
 
 	public static void main(String[] args) throws Exception {
 
-		Path p = Paths.get("src\\day27_pp1\\textDemo.txt");
-		Files.deleteIfExists(p);
-		add();
-		add();
+//		Path p = Paths.get("src\\day27_pp1\\textDemo.txt");
+//		add();
+//		add();
 		show();
-		Path d = Paths.get("src\\day27_pp1\\NewD");
-		System.out.println("File Exists: " + Files.exists(p));
-		Files.createFile(p);
-		Files.createDirectories(d);
-		Files.createFile(p);
-		Files.walk(Paths.get("C:\\Users\\TheUA\\eclipse-workspace\\com.bridge.week3\\src\\day27_pp1"))
-				.filter(Files::isRegularFile).forEach(System.out::println);
+		List<EmployeePayroll> t = ojbectFromFile();
+		System.out.println(t);
+//		Path d = Paths.get("src\\day27_pp1\\NewD");
+//		System.out.println("File Exists: " + Files.exists(p));
+//		Files.createDirectories(d);
+//		Files.createFile(p);
+//		Files.walk(Paths.get("C:\\Users\\TheUA\\eclipse-workspace\\com.bridge.week3\\src\\day27_pp1"))
+//				.filter(Files::isRegularFile).forEach(System.out::println);
 
 	}
 
@@ -32,23 +33,45 @@ public class EmployeeSystem {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		EmployeePayroll e = new EmployeePayroll();
 		System.out.println("Enter Details: ");
+		System.out.println("ID: ");
 		e.id = Integer.parseInt(br.readLine());
+		System.out.println("Name: ");
 		e.name = (br.readLine());
+		System.out.println("Salary: ");
 		e.salary = Integer.parseInt(br.readLine());
 		System.out.println(e.toString());
 		if (Files.exists(Paths.get("src\\day27_pp1\\textDemo.txt"))) {
 			Files.write(Paths.get("src\\day27_pp1\\textDemo.txt"), e.toString().getBytes(), StandardOpenOption.APPEND);
 		} else {
 			Files.createFile(Paths.get("src\\day27_pp1\\textDemo.txt"));
-			Files.write(Paths.get("src\\day27_pp1\\textDemo.txt"), e.toString().getBytes());
+			Files.write(Paths.get("src\\day27_pp1\\textDemo.txt"), "id, name, salary\n".getBytes());
+			Files.write(Paths.get("src\\day27_pp1\\textDemo.txt"), (e.toString() + "\n").getBytes(),
+					StandardOpenOption.APPEND);
 		}
 		count++;
 
 	}
-	
-	public static void show() throws Exception{
-		List<String> list =  Files.readAllLines(Paths.get("src\\day27_pp1\\textDemo.txt"));
-		list.stream().forEach(System.out :: println);
-		System.out.println("Count : "+list.stream().count());
+
+	public static void show() throws Exception {
+		List<String> list = Files.readAllLines(Paths.get("src\\day27_pp1\\textDemo.txt"));
+		list.stream().forEach(System.out::println);
+		System.out.println("Count : " + list.stream().count());
+	}
+
+	public static List<EmployeePayroll> ojbectFromFile() throws Exception {
+		List<String> list = Files.readAllLines(Paths.get("src\\day27_pp1\\textDemo.txt"));
+		List<EmployeePayroll> newList = new ArrayList<>();
+		for (int i = 1; i < list.size(); i++) {
+			//", " is the gap!
+			String[] line = list.get(i).split(", ");
+			EmployeePayroll e = new EmployeePayroll();
+			for (int j = 0; j < line.length; j++) {
+				e.id = Integer.parseInt(line[0]);
+				e.name = line[1];
+				e.salary = Integer.parseInt(line[2]);
+			}
+			newList.add(e);
+		}
+		return newList;
 	}
 }
